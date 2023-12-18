ccb8de2450a2adf8300f467c021a48cc8bb03773
florence11 <florence.crozat@epfl.ch>
2023-12-18 19:03:47 +0100

    test

---
+++ ada_website/_site/sw.js
@@ -0,0 +1,73 @@
+const version = '20231218190239';
+const cacheName = `static::${version}`;
+
+const buildContentBlob = () => {
+  return ["/ada_website/jekyll/update/2023/12/04/welcome-to-jekyll/","/ada_website/about/","/ada_website/","/ada_website/manifest.json","/ada_website/assets/search.json","/ada_website/assets/styles.css","/ada_website/redirects.json","/ada_website/feed.xml","/ada_website/sitemap.xml","/ada_website/robots.txt","", "/ada_website/assets/default-offline-image.png", "/ada_website/assets/scripts/fetch.js"
+  ]
+}
+
+const updateStaticCache = () => {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll(buildContentBlob());
+  });
+};
+
+const clearOldCache = () => {
+  return caches.keys().then(keys => {
+    // Remove caches whose name is no longer valid.
+    return Promise.all(
+      keys
+        .filter(key => {
+          return key !== cacheName;
+        })
+        .map(key => {
+          console.log(`Service Worker: removing cache ${key}`);
+          return caches.delete(key);
+        })
+    );
+  });
+};
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    updateStaticCache().then(() => {
+      console.log(`Service Worker: cache updated to version: ${cacheName}`);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clearOldCache());
+});
+
+self.addEventListener("fetch", event => {
+  let request = event.request;
+  let url = new URL(request.url);
+
+  // Only deal with requests from the same domain.
+  if (url.origin !== location.origin) {
+    return;
+  }
+
+  // Always fetch non-GET requests from the network.
+  if (request.method !== "GET") {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Default url returned if page isn't cached
+  let offlineAsset = "/offline/";
+
+  if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+    // If url requested is an image and isn't cached, return default offline image
+    offlineAsset = "/ada_website/assets/default-offline-image.png";
+  }
+
+  // For all urls request image from network, then fallback to cache, then fallback to offline page
+  event.respondWith(
+    fetch(request).catch(async () => {
+      return (await caches.match(request)) || caches.match(offlineAsset);
+    })
+  );
+  return;
+});
